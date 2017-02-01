270ae7fdf11908d884def92d1f51efb3a6224e58
sanDu <466452596@qq.com>
2017-02-02 00:22:42 +0800

    Update 正则.js

--- JavaScript Demo/正则.js
+++ JavaScript Demo/正则.js
@@ -4,3 +4,21 @@ function parseQuery(query) {
 }
 var tom = parseQuery("name=tom&age=12&gender&");
 	tom;//返回值：{name: "tom", age: "12", gender: ""}
+//版本二
+function escapeHTML(htmlStr){
+htmlStr = htmlStr.replace(/[<>&"]/g,function(m0) {
+	switch(m0){
+	case '<':
+	   return '&lt;';
+	case '>':
+	   return '&gt;';
+	case '&':
+	   return '&amp;';
+	case '"':
+	   return '&quot;';
+    };
+});
+return htmlStr;
+}
+var a = escapeHTML('<input type="text" name="mobile">');
+alert(a);
