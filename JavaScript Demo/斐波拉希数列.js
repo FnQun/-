7d8c1051284a65ef1684a7081dc158cde0a6b4ee
sanDu <466452596@qq.com>
2017-02-02 00:35:29 +0800

    Update 斐波拉希数列.js

--- JavaScript Demo/斐波拉希数列.js
+++ JavaScript Demo/斐波拉希数列.js
@@ -1,3 +1,21 @@
+function fibonacci(n){
+    var a,b,result;
+    if(n<=2){
+		if(n===0) {return 0;}
+		else if(n===1||2) {return 1;}
+    }
+    a = b = 1;
+    for(var i=3;i<=n;i++){
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
+}
+console.log(fibonacci(1));
+console.log(fibonacci(5));
+console.log(fibonacci(10));
+// version 2.0
 (function (value) {
                 if (value > 1)
                     return value *arguments.callee(value-1);
